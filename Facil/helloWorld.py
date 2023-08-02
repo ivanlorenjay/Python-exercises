c4c7ab60886b44fc06d25334c953c7397c10d087
Ivan Lorenzo <105089712+ivanlorenjay@users.noreply.github.com>
2023-08-01 21:40:00 -0600

    funcion print

---
+++ Facil/helloWorld.py
@@ -0,0 +1,8 @@
+""" Para el primer archivo de este nuevo proyecto: 
+La función print()
+se utiliza para imprimir un mensaje o un objeto en la salida estándar, que generalmente es la pantalla.
+"""
+
+print("Hola mundo!")
+
+print("Este es otro mensaje")
