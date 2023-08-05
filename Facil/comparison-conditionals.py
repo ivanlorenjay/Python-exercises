8dc627278a9918f966b5556ba8ccbe89150c6812
Ivan Lorenzo <105089712+ivanlorenjay@users.noreply.github.com>
2023-08-04 20:25:59 -0600

    cambio de nombre y agrego los 3 primeros if

---
+++ Facil/comparison-conditionals.py
@@ -0,0 +1,35 @@
+# Usando uno de los operadores de comparación en Python, escribe un programa simple de 
+# dos líneas que tome el parámetro n como entrada, que es un entero, e imprime `False` 
+# si `n` es menor que `100`, y `True` if `n` es mayor o igual que `100`.
+
+# No debes crear ningún bloque if (hablaremos de ellos muy pronto).
+
+n = float(input("Escribe un número: "))
+print(n >= 100)
+
+# ------------------------------------------------------------------------------------
+
+# if conditional  
+
+if ( True == True):
+    print("Is True!")
+
+# if-else conditional 
+
+if ( True == True ):
+    print("Is True!")
+else:
+    print("Is False!")
+
+# if nested
+
+if ( value > 0 ):
+    if ( value == 1 ):
+        print(Value)
+    else:
+        print("No 1")
+else:
+    if ( value == 0):
+        print(value)
+    else:
+        print("Value less than 0")
